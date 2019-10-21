510dec7430bed5e5a6d9a478e9b330627ede7fae
harika91 <harika91@users.noreply.github.com>
2019-10-21 08:36:42 -0500

    Initial commit

---
+++ src/main/java/edu/mcw/rgd/SampleQuery.java
@@ -0,0 +1,35 @@
+package edu.mcw.rgd;
+
+import edu.mcw.rgd.datamodel.pheno.Sample;
+import edu.mcw.rgd.datamodel.pheno.Study;
+import org.springframework.jdbc.object.MappingSqlQuery;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SampleQuery extends MappingSqlQuery {
+
+    public SampleQuery(DataSource ds, String query) {
+        super(ds, query);
+    }
+
+    protected Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+        Sample sample = new Sample();
+        sample.setId(rs.getInt("sample_id"));
+        sample.setAgeDaysFromHighBound(rs.getInt("age_days_from_dob_high_bound"));
+        sample.setAgeDaysFromLowBound(rs.getInt("age_days_from_dob_low_bound"));
+        sample.setNumberOfAnimals(rs.getInt("number_of_animals"));
+        sample.setNotes(rs.getString("sample_notes"));
+        sample.setSex(rs.getString("sex"));
+        sample.setStrainAccId(rs.getString("strain_ont_id"));
+        sample.setTissueAccId(rs.getString("tissue_ont_id"));
+        sample.setCellTypeAccId(rs.getString("cell_type_ont_id"));
+        sample.setSubcellComponentAccId(rs.getString("subcell_component_ont_id"));
+        sample.setGeoSampleAcc(rs.getString("geo_sample_acc"));
+        sample.setBioSampleId(rs.getString("biosample_id"));
+        return sample;
+    }
+
+}
