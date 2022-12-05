38fc702ef620b6b3df974c7036ec7d8aade83464
llamersmcw <llamers@mcw.edu>
2022-12-05 15:04:56 -0600

    updating life stage to include multiple stages

--- src/main/java/edu/mcw/rgd/UpdateLifeStage.java
+++ src/main/java/edu/mcw/rgd/UpdateLifeStage.java
@@ -29,17 +29,13 @@ static Logger log = LogManager.getLogger("lsStatus");
             for (Sample s : samples){
                 String lifeStage = calcLifeStage(s.getAgeDaysFromLowBound(), s.getAgeDaysFromHighBound());
                 if (!Utils.stringsAreEqual(s.getLifeStage(),lifeStage)) {
-                    log.info("\t\tSample_id: " + s.getId() + " | Old Life Stage: " + s.getLifeStage() + " | New Life Stage: " + lifeStage);
+                    log.info("\t\tSample_id: " + s.getId() + "| AgeLow (in days): " + s.getAgeDaysFromLowBound()
+                            + "| AgeHigh (in days): " + s.getAgeDaysFromHighBound()+" | Old Life Stage: " + s.getLifeStage() + " | New Life Stage: " + lifeStage);
                     s.setLifeStage(lifeStage);
                     updatedLS.add(s);;
                 }
-                if (lifeStage==null) {
-                    log.info("\t\t\tLife stage in question for sample_id=" + s.getId());
-                    overlappingStages++;
-                }
             }
             log.info("\tLife Stages being updated: " + updatedLS.size());
-            log.info("\tOverlapping Life Stages: "+overlappingStages);
             dao.updateLifeStageBatch(updatedLS);
 
             log.info("Update Life Stage end!");
@@ -60,36 +56,86 @@ static Logger log = LogManager.getLogger("lsStatus");
         * aged =  >=720 days
         * */
         String lsLow, lsHigh;
-        if (ageLow>=720)
-            lsLow = "aged";
-        else if (ageLow>=56)
-            lsLow = "adult";
-        else if (ageLow>=35)
-            lsLow ="juvenile";
-        else if (ageLow>=21)
-            lsLow = "weanling";
-        else if (ageLow>=0)
-            lsLow = "neonatal";
-        else
-            lsLow = "embryonic";
+        String ls = "";
 
-        if (ageHigh>=720)
-            lsHigh = "aged";
-        else if (ageHigh>=56)
-            lsHigh = "adult";
-        else if (ageHigh>=35)
-            lsHigh ="juvenile";
-        else if (ageHigh>=21)
-            lsHigh = "weanling";
-        else if (ageHigh>=0)
-            lsHigh = "neonatal";
-        else
-            lsHigh = "embryonic";
+        if (ageLow>=720 && ageHigh>=720)
+            ls = "aged";
+        else if (ageLow>=56 && ageHigh>=720)
+            ls= "adult;aged";
+        else if (ageLow>=35 && ageHigh>=720)
+            ls = "juvenile;adult;aged";
+        else if (ageLow>=21 && ageHigh>=720)
+            ls = "weanling;juvenile;adult;aged";
+        else if (ageLow>=0 && ageHigh>=720)
+            ls = "neonatal;weanling;juvenile;adult;aged";
+        else if (ageLow<0 && ageHigh>=720)
+            ls = "embryonic;neonatal;weanling;juvenile;adult;aged";
+        else if (ageLow>=56 && ageHigh>=56)
+            ls= "adult";
+        else if (ageLow>=35 && ageHigh>=56)
+            ls = "juvenile;adult";
+        else if (ageLow>=21 && ageHigh>=56)
+            ls = "weanling;juvenile;adult";
+        else if (ageLow>=0 && ageHigh>=56)
+            ls = "neonatal;weanling;juvenile;adult";
+        else if (ageLow<0 && ageHigh>=56)
+            ls = "embryonic;neonatal;weanling;juvenile;adult";
+        else if (ageLow>=35 && ageHigh>=35)
+            ls = "juvenile";
+        else if (ageLow>=21 && ageHigh>=35)
+            ls = "weanling;juvenile";
+        else if (ageLow>=0 && ageHigh>=35)
+            ls = "neonatal;weanling;juvenile";
+        else if (ageLow<0 && ageHigh>=35)
+            ls = "embryonic;neonatal;weanling;juvenile";
+        else if (ageLow>=21 && ageHigh>=21)
+            ls = "weanling";
+        else if (ageLow>=0 && ageHigh>=21)
+            ls = "neonatal;weanling";
+        else if (ageLow<0 && ageHigh>=21)
+            ls = "embryonic;neonatal;weanling";
+        else if (ageLow>=0 && ageHigh>=0)
+            ls = "neonatal";
+        else if (ageLow<0 && ageHigh>=0)
+            ls = "embryonic;neonatal";
+        else if (ageLow<0 && ageHigh<0)
+            ls = "embryonic";
 
-        if (Utils.stringsAreEqual(lsLow,lsHigh))
-            return lsLow;
+        if (ls.isEmpty())
+            return null;
         else
-            return null; //lsHigh+"/"+lsLow;
+            return ls;
+//
+//        if (ageLow>=720)
+//            lsLow = "aged";
+//        else if (ageLow>=56)
+//            lsLow = "adult";
+//        else if (ageLow>=35)
+//            lsLow ="juvenile";
+//        else if (ageLow>=21)
+//            lsLow = "weanling";
+//        else if (ageLow>=0)
+//            lsLow = "neonatal";
+//        else
+//            lsLow = "embryonic";
+//
+//        if (ageHigh>=720)
+//            lsHigh = "aged";
+//        else if (ageHigh>=56)
+//            lsHigh = "adult";
+//        else if (ageHigh>=35)
+//            lsHigh ="juvenile";
+//        else if (ageHigh>=21)
+//            lsHigh = "weanling";
+//        else if (ageHigh>=0)
+//            lsHigh = "neonatal";
+//        else
+//            lsHigh = "embryonic";
+//
+//        if (Utils.stringsAreEqual(lsLow,lsHigh))
+//            return lsLow;
+//        else
+//            return null; //lsHigh+";"+lsLow;
     }
 
     public void setSpecies(int species) {
